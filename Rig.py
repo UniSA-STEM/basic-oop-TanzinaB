8e91092cf70aa34e3dc65411ce40d67e3ffba091
TanzinaB <tanzina.billah@gmail.com>
2025-10-09 19:08:16 +1030

    Update Rig.py

--- Rig.py
+++ Rig.py
@@ -6,3 +6,16 @@ ID: 110458303
 Username: bilty009
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+from asset import Asset
+
+class Rig:
+  def __init__(self, name):
+    self.name = name
+    self.damage = 0
+    self.broken = False
+    self.upgrade_level = 0
+    self.storage = [
+      Asset("Data Spike", "Used to attack other rigs"),
+      Asset("Data Spike", "Used to attack other rigs"),
+      Asset("Removable Drive", "Used to extract unsecured assets")
+    ]
