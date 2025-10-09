049b9c5d009d64b6c2c47e0a3446ffcab8c40431
TanzinaB <tanzina.billah@gmail.com>
2025-10-09 19:02:12 +1030

    Update Asset.py

--- Asset.py
+++ Asset.py
@@ -6,3 +6,14 @@ ID: 110458303
 Username: bilty009
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+class Asset: 
+  def __init__(self, name, description):
+    self.name = name
+    self.description = description
+    self.encrypted = False
+
+  def __str__(self):
+    if self.encrypted:
+      return f"{self.name}: {self.description} [Encrypted]"
+    else:
+      return f"{self.name}: {self.description}"
