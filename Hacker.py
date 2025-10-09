c6969e2dd4fa4d1d1fbd851d035bc55822ec9113
TanzinaB <tanzina.billah@gmail.com>
2025-10-09 19:20:08 +1030

    Update Hacker.py
    Creating the structure for the hacker class with its attributes and methods.

--- Hacker.py
+++ Hacker.py
@@ -15,3 +15,26 @@ class Hacker:
     self.inventory = [Asset("CryptoToken, "Used to buy or repair rigs")]
     self.rig = None
     self.trace_level = 0
+
+  def acquire_rig(self):
+    pass
+
+  def attack(self, target):
+    pass
+
+  def extract_assets(self, target):
+    pass
+
+  def encrypt_asset(self, asset_name):
+    pass
+
+  def decrypt_asset(self, asset_name):
+    pass
+
+  def upgrade_rig(self):
+    pass
+
+  def __str__(self):
+    inv = -----
+    rig_name = self.rig.name if self.rig else "None"
+    return f"Hacker: {self.name}, Rig: {rig_name}, Trace Level: {self.trace_level}, Inventory: {inv}"
