5d7f527fec5b4f70c3b827f265eb5f7d74870e9e
Tanzina Billah <tanzina.billah@gmail.com>
2025-10-13 20:35:18 +1030

    Putting comments in the Asset Class

--- Asset.py
+++ Asset.py
@@ -6,11 +6,18 @@ ID: 110458303
 Username: bilty009
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
-class Asset: 
-  def __init__(self, name, description):
+
+    # This class represents a digital asset
+class Asset:
+    def __init__(self, name, description):
+
+    #Each asset has a name and a description#
     self.name = name
     self.description = description
-    self.encrypted = False
+    self.encrypted = False  #By default, an asset starts unencrypted#
+
+    # This method returns the description of the asset
+    # If the asset is encrypted, that is shown in the text
 
   def __str__(self):
     if self.encrypted:
