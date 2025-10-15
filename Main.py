7f685203d6257b616c28255f470b1b88c764aa0c
Tanzina Billah <tanzina.billah@gmail.com>
2025-10-15 21:46:31 +1030

    Writing usual test cases for the main file

--- Main.py
+++ Main.py
@@ -12,11 +12,27 @@ from asset import Asset
 h1 = Hacker("NeonRaven")
 h2 = Hacker("CyberWolf")
 
-h1.acquire_rig()
-h2.acquire_rig()
+h1.acquire_rig(self)
+h2.acquire_rig(self)
 
 print(h1)
 print(h2)
 
 h1.inventory.append(Asset("Security Chip", "Used to encrypt/decrypt"))
 h1.inventory.append(Asset("Hardware Patch", "Used to upgrade rigs"))
+
+h1.upgrade_rig(self)
+print(h1.rig)
+
+h1.attack(h2)
+h1.attack(h2)
+print(h2.rig)
+
+h1.extract_assets(h2)
+print(h1)
+
+h1.encrypt_asset("Security Chip")
+h1.decrypt_asset("Security Chip")
+
+print(h1)
+print(h2)
